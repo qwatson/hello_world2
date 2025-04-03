7f9c076c78d779095dd0566802861afff551293c
Quentin <quentin.watson@protonmail.com>
2025-04-03 10:53:36 -0500

    Imported JOptionPane. Changed output to GUI. Added inputs for name, age, and favorite food. Created outputs saying user's name, age, and favorite food.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        String name = JOptionPane.showInputDialog("What is your name?");
+        JOptionPane.showMessageDialog(null, "Hello world and " + name + "!");
 
+        int age = Integer.parseInt(JOptionPane.showInputDialog("How many years old are you?"));
+        String food = JOptionPane.showInputDialog("What is your favorite food?");
+
+        JOptionPane.showMessageDialog(null, "Your name is " + name + ".\nYou are " + age + " years old.\n" + "Your favorite food is " + food + "." );
+        JOptionPane.showMessageDialog(null, "It is nice to meet you " + name + "!");
     }
 }
